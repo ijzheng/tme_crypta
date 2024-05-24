8d572710d33f5d35f6ad9ade0120f71e913585c6
ijzheng <102190198+ijzheng@users.noreply.github.com>
2024-05-24 04:36:44 +0200

    svp

--- svp_problem.py
+++ svp_problem.py
@@ -6,20 +6,16 @@ p = int("0x372bb4e2c6ba2d5a", 16) # valeur p
 a = int("0x225f565888c685fb", 16)  # valeur a
 L = 2**54.37  # borne supérieure L
 
-# Définir la matrice A
 A = IntegerMatrix(n, n)
 A[0, 0] = 1
 for i in range(1, n):
     A[i, i] = p
     A[0, i] = pow(a, i, p)
 
-# Appliquer l'algorithme LLL pour trouver un vecteur court dans le réseau
 A_LLL = LLL.reduction(A)
 
-# Obtenir le vecteur court trouvé
 short_vector = A_LLL[0]
 
-# Vérifier si la norme du vecteur est inférieure à L
 norm_short_vector = short_vector.norm()
 if norm_short_vector < L:
     print("Le vecteur court trouvé est :", short_vector)
